59ddb57b8903e62fd6e856c7755ac85312c8fbda
christian-paul <christiansamuelpaul@gmail.com>
2017-03-31 01:21:43 +0000

    add register player, count players, delete players, report match, delete matches, player standings

--- tournament.py
+++ tournament.py
@@ -13,14 +13,28 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('delete from matches;')
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('delete from players;')
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute('select count(*) from players;')
+    return c.fetchone()[0]
 
 
 def registerPlayer(name):
@@ -32,6 +46,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute('insert into players (name) values (%s);', (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +66,15 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("""select players.id, players.name,
+              (select count(*) from matches where matches.winner = players.id) as wins,
+              count(matches.winner) as matches
+              from players left join matches on players.id = matches.winner or players.id = matches.loser
+              group by players.id
+              order by wins;""")
+    return c.fetchall()
 
 
 def reportMatch(winner, loser):
@@ -56,7 +84,12 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    conn = connect()
+    c = conn.cursor() 
+    c.execute('insert into matches (winner, loser) values (%s, %s);', (winner, loser))
+    conn.commit()
+    conn.close()
+
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
--- tournament_test.py
+++ tournament_test.py
@@ -61,7 +61,8 @@ def testStandingsBeforeMatches():
         raise ValueError("Each playerStandings row should have four columns.")
     [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
     if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0:
-        raise ValueError(
+        print standings
+	raise ValueError(
             "Newly registered players should have no matches or wins.")
     if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
         raise ValueError("Registered players' names should appear in standings, "
@@ -90,6 +91,7 @@ def testReportMatches():
         if i in (id1, id3) and w != 1:
             raise ValueError("Each match winner should have one win recorded.")
         elif i in (id2, id4) and w != 0:
+	    print standings
             raise ValueError("Each match loser should have zero wins recorded.")
     print "7. After a match, players have updated standings."
     deleteMatches()
